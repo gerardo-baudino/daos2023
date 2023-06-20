c53541831f523244d8d21668d3f40a526965d6e8
Romina <rominapozzuto@gmail.com>
2023-06-20 20:56:23 -0300

    integro servicios del cliente

--- src/main/java/com/tsti/entity/Client.java
+++ src/main/java/com/tsti/entity/Client.java
@@ -1,11 +1,14 @@
 package com.tsti.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
 public class Client {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private Long document;
     private String firstName;
